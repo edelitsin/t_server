bc64a0a9a53c138899f028a7ac447f846448b015
Evgeny Delitsin <jenok@bk.ru>
2023-03-20 18:03:25 +0300

    qa

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
-sudo mv /home/box/t_server/web ~
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo pip3 install virtualenv
+sudo pip3 install pathlib2
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install django
+
 sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+
 sudo /etc/init.d/nginx restart
+
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:wsgi_app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
+#sudo mv /home/box/t_server/web ~
