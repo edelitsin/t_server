1c881ebf396dd5089e6257c2c4c7eda7ce3ace5a
Evgeny Delitsin <jenok@bk.ru>
2023-03-20 19:11:08 +0300

    1

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-gunicorn -w 2 -c /home/box/web/etc/hello.py hello:wsgi_app #& gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app
