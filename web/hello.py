983d18b5e3e96a8ddda9ce6186631236dfff4a9b
Evgeny Delitsin <jenok@bk.ru>
2023-03-20 17:57:51 +0300

    hello

--- web/hello.py
+++ web/hello.py
@@ -1,6 +1,6 @@
 def wsgi_app(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
     start_response(status, headers)
     return body
